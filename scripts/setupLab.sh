a804a355d7086a7a0dedb922eb7b9ec5a120bd5b
David Carew <carew@us.ibm.com>
2020-03-22 20:46:29 -0500

    Minor tweaks based on lab walkthrough

--- scripts/setupLab.sh
+++ scripts/setupLab.sh
@@ -9,52 +9,70 @@ fi
 
 usage () {
   echo "Usage:"
-  echo "setupLab.sh URL_TO_SETUP_FILES  (if env var USERNAME is set)"
+  echo "setupLab.sh URL_TO_SETUP_FILES  (Requires env var USERNAME to be set)"
   echo "or"
-  echo "setupLab.sh URL_TO_SETUP_FILES [CLUSTER_NAME] (if env var USERNAME  is set)"
+  echo "setupLab.sh URL_TO_SETUP_FILES [CLUSTER_NAME] (Requires env var USERNAME to be set)"
   echo "or"
   echo "setupLab.sh URL_TO_SETUP_FILES [CLUSTER_NAME] [KAFKA_TOPIC_NAME]"
 }
 
-if [ -z "$1" ]
+if [[ "$#" -lt 1 ]] || [[ "$#" -gt 3 ]]
 then
     usage
     exit 1
 fi
 
 
-if [ -z "$2" ]
+if [ "$#" -eq 1 ]
 then
-    if [ -z "$USERNAME" ]
+    if [[ -z "$USERNAME" ]] && [[ -z "$STUDENTID" ]]
     then
-       echo "Error: USERNAME env var not set and CLUSTER_NAME not supplied"
+       echo "Error: USERNAME or STUDENTID env vars not set and CLUSTER_NAME not supplied"
        usage
        exit 1
     else
-       CLUSTER_NAME=$USERNAME-cluster
+       if [ -n "$STUDENTID" ]
+       then
+         CLUSTER_NAME=$STUDENTID-cluster
+         KAFKA_TOPIC=stocktrader-$STUDENTID
+       else
+         CLUSTER_NAME=$USERNAME-cluster
+         KAFKA_TOPIC=stocktrader-$USERNAME
+       fi
     fi
-else
-    CLUSTER_NAME=$2
 fi
 
-if [ -z "$3" ]
+if [ "$#" -eq 2 ]
 then
-  if [ -z "$USERNAME" ]
+  if [[ -z "$USERNAME" ]] && [[ -z "$STUDENTID" ]]
   then
-     echo "Error: USERNAME env var not set and KAFKA_TOPIC_NAME not supplied"
+     echo "Error: USERNAME or STUDENTID env vars not set and KAFKA_TOPIC_NAME not supplied"
      usage
      exit 1
   else
-     KAFKA_TOPIC=stocktrader-$USERNAME
+     CLUSTER_NAME=$2
+     if [ -n "$STUDENTID" ]
+     then
+       KAFKA_TOPIC=stocktrader-$STUDENTID
+     else
+       KAFKA_TOPIC=stocktrader-$USERNAME
+     fi
   fi
-else
-    KAFKA_TOPIC=$3
 fi
 
-echo "Using $CLUSTER_NAME as IKS cluster name ..."
+if [ "$#" -eq 3 ]
+then
+  CLUSTER_NAME=$2
+  KAFKA_TOPIC=$3
+fi
+
+echo "Using $CLUSTER_NAME as IKS cluster name"
+echo "Using $KAFKA_TOPIC as Kakfa Topic name"
+
+echo "Validating setup URL ..."
 regex='(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 if [[ ! $1 =~ $regex ]]; then
-   echo "Error: Argument is not a valid URL"
+   echo "Error: Argument $1 is not a valid URL"
    exit 1
 fi
 
@@ -74,21 +92,17 @@ else
 fi
 
 echo "Getting Ingress subdomain for cluster $CLUSTER_NAME  ..."
-#ibmcloud ks cluster-get --cluster $CLUSTER_NAME > tmp.out
 
 ingress_subdomain=`ibmcloud ks cluster-get --cluster $CLUSTER_NAME | grep "Ingress Subdomain:" | awk '{print $3}'`
 
 if [ $? -ne 0 ]
 then
     echo "Error: Retrieving ingress subdomain.  Redo cluster access setup and try again"
-    #rm tmp.out
     exit 1
 fi
 
-#ingress_subdomain=`cat tmp.out | grep "Ingress Subdomain:" | awk '{print $3}' `
-#rm tmp.out
 
-echo "Updating Helm chart with ingress subdomain: $ingress_subdomain"
+echo "Updating Stock Trader Helm chart with ingress subdomain: $ingress_subdomain"
 sed -i"_x" "s/changeme/$ingress_subdomain/g" ../stocktrader/values.yaml && rm ../stocktrader/values.yaml_x
 
 echo "Updating variables.sh with Kafka topic : $KAFKA_TOPIC"
